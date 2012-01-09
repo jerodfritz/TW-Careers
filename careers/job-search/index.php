3f852459a661d7e20dcc8f2afa69dbb168ac3cc4
Jerod <jerod@centogram.com>
2012-01-08 20:54:13 -0500

    updated job search with multi select and workable location search

--- careers/job-search/index.php
+++ careers/job-search/index.php
@@ -10,7 +10,7 @@ $dir = '/careers/job-search/';
 $stylesheets = array(
   $dir.'css/jquery.multiselect.css', 
   'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/themes/flick/jquery-ui.css',
-  $dir.'css/careers.css', );
+  $dir.'css/job-search.css', );
 
 $javascripts = array(
   'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', 
@@ -25,94 +25,104 @@ $ks = new KenexaSearch();
 
 // Do we have any question inputs?
 if (isset($_REQUEST['questions'])) {
-	// Add each one to the search.
-	foreach ($_REQUEST['questions'] as $key => $value) {
-		if ($value !== "") {
-			$qId = substr($key, strpos($key, '_') + 1);
-			// Get the number part of the input name (the questionId).
-			$ks -> addQuestion($qId, $value);
-		}
-	}
+    // Add each one to the search.
+    foreach ($_REQUEST['questions'] as $key => $value) {
+        if ($value !== "") {
+            $qId = substr($key, strpos($key, '_') + 1);    // Get the number part of the input name (the questionId).            
+            $ks->addQuestion($qId, $value);
+        }
+    }
+  //$ks->addQuestion(KenexaJobQuestions::LOCATION,"Paris - France");
 }
-
 $isAjax = false;
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-	$isAjax = true;
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) &&
+        strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $isAjax = true;
 }
 
 ob_start();
 
 if (!$isAjax) {
-	// Create a bunch of inputs based on the big XML file.
-	echo "<form id='searchForm' action = '#' method='GET'>";
-	$fields = $ks -> getFields();
-	foreach ($fields as $key => $field) {
-		$keyId = 'kenexaq' . '_' . $key;
-		echo "<label for='$keyId'>" . $field['Name'] . "</label>";
-		$type = strtolower($field['Type']);
-		switch ($type) {
-			case 'radio' :
-			case 'multi-select' :
-			case 'select' :
-				echo "<select class='kenexa-question' id=\"$keyId\"  name='questions[$keyId]' >";
-				echo "<option value='*'>Any</option>";
-				foreach ($field['options'] as $option) {
-					echo "<option>{$option['Code']}</option>";
-				}
-				echo "</select><br/>";
-				break;
-			case 'text' :
-				echo "<input class='kenexa-question' id=\"$keyId\" name='questions[$keyId]'></input><br/>";
-				break;
-			case 'textarea' :
-				echo "<textarea class='kenexa-question' id=\"$keyId\" name='questions[$keyId]'></textarea><br/>";
-				break;
-			default :
-				echo "<input class='kenexa-question' id=\"$keyId\" name='questions[$keyId]'></input><br/>";
-		}
-	}
+    // Create a bunch of inputs based on the big XML file.
+    echo "<form id='searchForm' action = 'index.php' method='POST'>";
+    $fields = $ks->getFields();
+    foreach ($fields as $key => $field) {
+        $keyId = 'kenexaq' . '_' . $key;
+        $label = "<label class='kenexa-label' for='$keyId'>" . $field['Name'] . "</label>";
+        $type = strtolower($field['Type']);
+        switch ($type) {
+            case 'radio':
+            case 'multi-select':
+            case 'select':
+                // If it is the location input, put a hidden one in there,
+                // as this will be change via the special location widget.
+                if($key == KenexaJobQuestions::LOCATION) {
+                    echo "<input type=\"hidden\" class='kenexa-question' id=\"$keyId\"  name='questions[$keyId]'></input><br/>";
+                }else {
+                    echo "$label<select class='kenexa-question' id=\"$keyId\"  name='questions[$keyId]' >";
+                    echo "<option value='*'>Any</option>";
+                    foreach ($field['options'] as $option) {
+                        echo "<option>{$option['Code']}</option>";
+                    }
+                    echo "</select><br/>";
+                }
+                break;
+            case 'text':
+                echo "$label<input class='kenexa-question' id=\"$keyId\" name='questions[$keyId]'></input><br/>";
+                break;
+            case 'textarea':
+                echo "$label<textarea class='kenexa-question' id=\"$keyId\" name='questions[$keyId]'></textarea><br/>";
+                break;
+            default:
+                echo "$label<input class='kenexa-question' id=\"$keyId\" name='questions[$keyId]'></input><br/>";
+        }
+    }
 
-	/*
-	 // Create a form and bunch of inputs based on the scraper data.
-	 $questions = $ks->getOptions();
-	 echo "<p><strong>Enter your search criteria...</strong></p>";
-	 echo "<form id='searchForm' action = 'index.php' method='POST'>";
-	 foreach($questions as $key => $questionId) {
-	 $keyId = $key .'_'.$questionId;
-	 echo    "<label for='$keyId'>$key</label>".
-	 "<input type='text' class='kenexa-question' name='questions[$keyId]' id = '$keyId'><br/>";
-	 }
-	 */
-	echo "<input type='submit' value='Submit'> - or - <input id ='ajaxSubmit' type='button' value='Submit AJAX'></form>";
 
-	echo "<div id='results'>";
+    /* 		
+      // Create a form and bunch of inputs based on the scraper data.
+      $questions = $ks->getOptions();
+      echo "<p><strong>Enter your search criteria...</strong></p>";
+      echo "<form id='searchForm' action = 'index.php' method='POST'>";
+      foreach($questions as $key => $questionId) {
+      $keyId = $key .'_'.$questionId;
+      echo    "<label for='$keyId'>$key</label>".
+      "<input type='text' class='kenexa-question' name='questions[$keyId]' id = '$keyId'><br/>";
+      }
+     */
+    echo "<input id ='ajaxSubmit' type='button' value='Search Jobs'></form>";
+
+    echo "<div id='results'>";
 }
 
 // Display search results.
 echo "<hr/>";
 
-$arr = $ks -> search();
+
+
+$arr = $ks->search();
 
 if ($arr) {
-	echo "<strong>Total jobs found: " . $arr -> OtherInformation -> TotalRecordsFound . "</strong><br/>";
-	$jobs = $arr -> Jobs -> Job;
-	foreach ($jobs as $job) {
-		echo "<h3>" . $job -> Question[KenexaJobData::JOB_TITLE] . "</h3>";
-		echo "Location: " . $job -> Question[KenexaJobData::LOCATION] . "<br/>";
-		//echo $job->HotJob."<br/>";
-		echo "Last updated: " . $job -> LastUpdated . "<br/>";
-		echo "<p>" . nl2br($job -> JobDescription) . "</p>";
-		echo "<a href=\"$job->JobDetailLink\">Click here for more details</a>";
-	}
+    echo "<strong>Total jobs found: " . $arr->OtherInformation->TotalRecordsFound . "</strong><br/>";
+    $jobs = $arr->Jobs->Job;
+    foreach ($jobs as $job) {
+        echo "<h3>" . $job->Question[KenexaJobData::JOB_TITLE] . "</h3>";
+        echo "Location: " . $job->Question[KenexaJobData::LOCATION] . "<br/>";
+        //echo $job->HotJob."<br/>";
+        echo "Last updated: " . $job->LastUpdated . "<br/>";
+        echo "<p>" . nl2br($job->JobDescription) . "</p>";
+        echo "<a href=\"$job->JobDetailLink\">Click here for more details</a>";
+    }
 }
 
 if ($isAjax) {
-	// If ajax, just return the html of the search results.
-	ob_end_flush();
-	exit();
+    // If ajax, just return the html of the search results.
+    ob_end_flush();
+    exit();
 }
 // Close the results div.
 echo "</div>";
+
 
 //   ["HotJob"]=>string(2) "No"
 //      ["LastUpdated"]=>
@@ -121,16 +131,16 @@ echo "</div>";
 
 /*
  * ["OtherInformation"]=>
- object(SimpleXMLElement)#8 (4) {
- ["TotalRecordsFound"]=>
- string(2) "32"
- ["MaxPages"]=>
- string(1) "1"
- ["StartDoc"]=>
- string(1) "1"
- ["PageNumber"]=>
- string(1) "1"
- }
+  object(SimpleXMLElement)#8 (4) {
+  ["TotalRecordsFound"]=>
+  string(2) "32"
+  ["MaxPages"]=>
+  string(1) "1"
+  ["StartDoc"]=>
+  string(1) "1"
+  ["PageNumber"]=>
+  string(1) "1"
+  }
  */
 $content = ob_get_contents();
 ob_end_clean();
@@ -150,23 +160,13 @@ ob_end_clean();
 			</div>
 		</div>
 		<div id="content">
-			<div class="content-holder">
-				<div id="content-main">
-                    <h1>Careers</h1>				
+			<div class="content-width">
 				<?php include $basePath . 'includes/structure/breadcrumb.php'; ?>
+				<div id="">
+                    <h1>Careers</h1>				
 				<div id="careers-advanced-search">
-					<div id="ajax-loader"><img src="<?php print $dir; ?>images/ajax-loader.gif"/>
-					</div>
-					<div id="location-selects">
-						<p>
-							Location widgets test (does not perform search)
-						</p>
-						<select id="country-select" ></select>
-						<select id="state-select" class="loc-select" size="6" multiple="multiple"></select>
-						<select id="city-select" class="loc-select" size="6" multiple="multiple"></select>
-					</div>
-					<?php echo $content; ?>
-					</div>
+					<div id="ajax-loader"><img src="<?php print $dir; ?>images/ajax-loader.gif"/></div>
+					<?php echo $content; ?>		
 				</div>
 			</div>
 		</div>
--- careers/job-search/js/job-search.js
+++ careers/job-search/js/job-search.js
@@ -1,93 +1,144 @@
 $(function() {
-	var createLocationSelects = function() {
-		// Current selected country.
-		var currentCountry;
+      var $locationWidget = $(
+        '<div id="location-selects">'+
+            '<p>Select desired location:</p>'+
+            '<select id="country-select" ></select>'+
+            '<select id="state-select" class="loc-select" size="6" multiple="multiple"></select>'+
+            '<select id="city-select" class="loc-select" size="6" multiple="multiple"></select>'+
+        '</div>');
+        $locationWidget.insertAfter('#kenexaq_1140');
+        
+       // alert($('#kenexaq_1140').length);
+        var createLocationSelects = function() {
+            // Current selected country.
+            var currentCountry,
+			
+            // Fills the states widget based on country.
+                fillStates = function(country) {
+                var states = locData[country],
+                    str="<option value='All'>All</option>";
+                // Pull out states for selected country.
+                for(var state in states) {
+                    if(state!= "NO_STATE") {
+                        str+= "<option value='" + state + "'>" + state + "</option>";
+                    }
+                }
+                $('#state-select').html(str);
+                $('#state-select').val("All");  // Select the first.
+                $('#state-select').multiselect("destroy").multiselect({header:false,selectedText:'States'});
+            },
+            
+            // Fills the cities widged based on states.
+            fillCities = function(country,states) {
+                // If All option, then fill array with all states.
+                if(states[0]=="All") {
+                    states=[];
+                    for(var state in locData[country])
+                        states.push(state);
+                }
+                // Run through states and pull out cities, and create the option values.
+                var str = "<option value='All'>All</option>";
+                for(var i=0;i<states.length;i++) {
+                    var cities = locData[country][states[i]];
+                    for(var c=0; c<cities.length;c++) {
+                        str += "<option value='";
+                            if(states[i] != "NO_STATE")  str += states[i] + " - ";
+                            str += cities[c] + " - " + country + "'>" + 
+                            cities[c] + "</option>";
+                    }
+                }
+                $('#city-select').html(str);
+                $('#city-select').val("All");   // Select the first.
+                $('#city-select').multiselect("destroy").multiselect({header:false,selectedText:'Cities'});
+            },
+            // Setup initial widgets, defaulting to 1st country.		
+            str="";
+            for(var country in locData) {
+                
+                if(currentCountry==undefined) currentCountry = country;
+                str+= "<option value='" + country + "'>" + country + "</option>";
+            }
+            $('#country-select').html(str);
+            // Fill in starting states and cities.
+            fillStates(currentCountry);
+            fillCities(currentCountry,$("#state-select").val());
+			
+            // On country change, fill in states and cities.
+            $('#country-select').bind('change',function(evt){
+                currentCountry = $(evt.target).val();
+                fillStates(currentCountry);
+                fillCities(currentCountry,$("#state-select").val());
+            });
+					
+            // On state change, fill in cities.
+            $('#state-select').bind('change',function(evt){
+                // Default to all states if nothing selected.
+                if (!$(evt.target).val()) {
+                    $(evt.target).val("All");
+                    $('#state-select').multiselect("destroy").multiselect({header:false,selectedList:1,selectedText:"States"});
+                }
+                fillCities(currentCountry,$(evt.target).val());	
+            });
+            $('#city-select').bind('change',function(evt){
+                // Default to all cities if nothing selected.
+                if (!$(evt.target).val()) {
+                    $(evt.target).val("All");
+                    $('#city-select').multiselect("destroy").multiselect({header:false,selectedText:"Cities"});
+                }
+            });	
+        }();
+        
+        $('#country-select').multiselect({multiple:false,header:false,selectedList:1});
 
-		// Fills the states widget based on country.
-		var fillStates = function(country) {
-			var states = locData[country], str = "<option value='All'>All</option>";
-			// Pull out states for selected country.
-			for(var state in states) {
-				if(state != "NO_STATE") {
-					str += "<option value='" + state + "'>" + state + "</option>";
-				}
-			}
-			$('#state-select').html(str);
-			$('#state-select').val("All");
-			// Select the first.
-		}
-		// Fills the cities widged based on states.
-		var fillCities = function(country, states) {
-			// If All option, then fill array with all states.
-			if(states[0] == "All") {
-				states = [];
-				for(var state in locData[country])
-				states.push(state);
-			}
-			// Run through states and pull out cities.
-			var str = "<option value='All'>All</option>";
-			for(var i = 0; i < states.length; i++) {
-				var cities = locData[country][states[i]];
-				for(var c = 0; c < cities.length; c++) {
-					str += "<option value='" + cities[c] + "'>" + cities[c] + "</option>";
-				}
-			}
-			$('#city-select').html(str);
-			$('#city-select').val("All");
-			// Select the first.
-		};
-		// Setup initial widgets, defaulting to 1st country.
-		var str = "";
-		for(var country in locData) {
-
-			if(currentCountry == undefined)
-				currentCountry = country;
-			str += "<option value='" + country + "'>" + country + "</option>";
-		}
-		$('#country-select').html(str);
-		// Fill in starting states and cities.
-		fillStates(currentCountry);
-		fillCities(currentCountry, $("#state-select").val());
-
-		// On country change, fill in states and cities.
-		$('#country-select').bind('change', function(evt) {
-			currentCountry = $(evt.target).val();
-			fillStates(currentCountry);
-			fillCities(currentCountry, $("#state-select").val());
-		});
-		// On state change, fill in cities.
-		$('#state-select').bind('change', function(evt) {
-			fillCities(currentCountry, $(evt.target).val());
-		});
-	}();
-
-	var ajaxBusy = false;
-	$('#ajaxSubmit').bind('click', function() {
-		var params = "";
-		if(ajaxBusy) {
-			return;
-		}
-		ajaxBusy = true;
-		$('.kenexa-question', $('#searchForm')).each(function() {
-			params += $(this).attr('name') + '=' + $(this).val() + "&";
-		});
-		$('#ajax-loader').css('display', 'block');
-		$('#results').html("<hr/>");
-		$.ajax({
-			type : "POST",
-			url : "index.php",
-			data : params,
-			dataType : "HTML",
-			success : function(data) {
-				$('#results').html(data);
-				$('#ajax-loader').css('display', 'none');
-				ajaxBusy = false;
-			},
-			error : function() {
-				$('#ajax-loader').css('display', 'none');
-				ajaxBusy = false;
-				alert('An error occurred.');
-			}
-		})
-	});
-});
+        // Creates a search string based on the location inputs,
+        // in CSV format for the Kenexa query value.
+        var createLocSearchCSV = function() {
+            var selectedCities = $("#city-select").val(),
+                query = "";
+            // If all cities selected, go through all select options and add them to query.
+            if (selectedCities[0] == "All") {
+                $('#city-select option').each(function(){
+                    if ($(this).attr('value') !== "All") query += $(this).attr('value') + ','; 
+                })
+            }else
+            // If specific cities selected, only add those to query.
+            for(var i=0;i<selectedCities.length;i++) {
+                query += selectedCities[i] + ',';
+            }
+            // Return query with trailing comma removed.
+            return query.slice(0, -1);
+        }
+        
+				
+        var ajaxBusy = false;
+        $('#ajaxSubmit').bind('click',function(){
+            // Fill in the location input from the value of the special location widgets.
+            $('#kenexaq_1140').val(createLocSearchCSV());
+            var   params = "";
+            if(ajaxBusy) {
+                return;
+            }
+            ajaxBusy = true;
+            $('.kenexa-question',$('#searchForm')).each(function(){
+                params += $(this).attr('name') +'=' + $(this).val() + "&";
+            });
+            $('#ajax-loader').css('display','block');
+            $('#results').html("<hr/>");
+            $.ajax({
+                type: "POST",
+                url: "index.php",
+                data: params,
+                dataType:"HTML",
+                success: function(data) {
+                    $('#results').html(data);
+                    $('#ajax-loader').css('display','none');
+                    ajaxBusy = false;
+                },
+                error: function() {
+                    $('#ajax-loader').css('display','none');
+                    ajaxBusy = false;
+                    alert('An error occurred.');
+                }
+            })
+        });
+    });
