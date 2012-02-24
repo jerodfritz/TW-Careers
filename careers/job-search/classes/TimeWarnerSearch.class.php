5aa96238d18a175dd24e36e4bd4e631d296ad57a
Jerod <jerod@centogram.com>
2012-02-24 14:32:43 -0500

    Fixed Table Columns to maintain a constant width between browsers and animations

--- careers/job-search/classes/TimeWarnerSearch.class.php
+++ careers/job-search/classes/TimeWarnerSearch.class.php
@@ -132,105 +132,102 @@ class TimeWarnerSearch {
 }
 
   function displaySearchResults() {
-    $arr = $this->ks->search();
-    if ($arr) {
-            $questions = array( "title"=>"Posting Job Title",
-                                "location"=>"Location",
-                                "division"=>"Division",
-                                "area_of_interest"=>"Area of Interest",
-                                "position"=>"Type",
-                                "req"=>"Req #",
-                                "date"=>"Date");
-            $sortBy = "date";
-            if (isset($_REQUEST['sortby'])) $sortBy = $_REQUEST['sortby'];
-            
-			$maxPages = intval($arr->OtherInformation->MaxPages);
+	$arr = $this->ks->search();
+	if ($arr) {
+		$questions = array( "title"=>"Posting Job Title",
+		        "location"=>"Location",
+		        "division"=>"Division",
+		        "area_of_interest"=>"Area of Interest",
+		        "position"=>"Type",
+		        "req"=>"Req #",
+		        "date"=>"Date");
+		$sortBy = "date";
+		if (isset($_REQUEST['sortby'])) $sortBy = $_REQUEST['sortby'];
+		
+		$maxPages = intval($arr->OtherInformation->MaxPages);
+		
+		echo "<div id='search-stats'>";
+		
+		// Show the search results stats.
+		
+		$resultsPerPage = 50;
+		$page = intval($arr->OtherInformation->PageNumber) ;
+		$total = intval($arr->OtherInformation->TotalRecordsFound);
+		$to = $page * $resultsPerPage;
+		$to = ($to > $total)? $total : $to ;
+		$from = (($page-1) * $resultsPerPage ) + 1;
+		
+		if($total > 0) {
+			echo sprintf( '<div class="results-count">Showing %s - %s of %s Results</div>', $from,  $to, $total);
 			
-			echo "<div id='search-stats'>";
-			
-				// Show the search results stats.
-
-				$resultsPerPage = 50;
-				$page = intval($arr->OtherInformation->PageNumber) ;
-				$total = intval($arr->OtherInformation->TotalRecordsFound);
-				$to = $page * $resultsPerPage;
-				$to = ($to > $total)? $total : $to ;
-				$from = (($page-1) * $resultsPerPage ) + 1;
-				
-				if($total > 0) {
-                  echo sprintf( '<div class="results-count">Showing %s - %s of %s Results</div>', $from,  $to, $total);				} else {
-                  echo '<div class="results-count">Your Search returned 0 Results</div>';
-                }
-
-				// If more than one page, show a link for each page.
-				// JS searches for these and adds events to do a paginated search.
-				if($maxPages > 1) {
-					echo "<div class='page-info'>";
-					//echo "<strong>Pages: </strong>";
-					if($this->ks->pageNumber > 1) {
-                      $num = $this->ks->pageNumber - 1;
-  					  echo "<div class='page-link$class' num='$num'>Previous</div>";
-                    }
-					for($i=1;$i<=$maxPages;$i++) {
-						$class="";
-						if($i == $this->ks->pageNumber ) $class = " page-link-current";	// additional class for current page.
+			// If more than one page, show a link for each page.
+			// JS searches for these and adds events to do a paginated search.
+			if($maxPages > 1) {
+				echo "<div class='page-info'>";
+				if($this->ks->pageNumber > 1) {
+					$num = $this->ks->pageNumber - 1;
+					echo "<div class='page-link$class' num='$num'>Previous</div>";
+				}
+				for($i=1;$i<=$maxPages;$i++) {
+					$class="";
+					if($i == $this->ks->pageNumber ) $class = " page-link-current";
+						// additional class for current page.
 						echo "<div class='page-link$class' num='$i'>$i</div>";
 					}
 					if($this->ks->pageNumber < $maxPages) {
-					  $num = $this->ks->pageNumber + 1;
-  					  echo "<div class='page-link$class' num='$num'>Next</div>";
+						$num = $this->ks->pageNumber + 1;
+						echo "<div class='page-link$class' num='$num'>Next</div>";
 					}
 					echo "</div>";		
 				}
 			echo "</div>";
-			
-            echo '<table id="results-table" style="clear:both" ><tr class="header">';
-            foreach($questions as $question=>$text) {
-                $class="";
-                if ($question != "req" && $question != "title") {
-                    $class="hover-effect"; 
-                    if ($sortBy == $question) {
+			echo '<table id="results-table" style="clear:both" ><tr class="header">';
+			foreach($questions as $question=>$text) {
+				$class="";
+				if ($question != "req" && $question != "title") {
+					$class="hover-effect"; 
+					if ($sortBy == $question) {
 						$class.= " sort-on-this";
 						if ($this->ks->sortDir == "ASC") $class.= " sort-asc";
 						else $class.= " sort-desc";
-						//echo $question."  ".$class;
 					}
-                }
-                echo "<th id='$question' class='$class'>$text<span class='sort-arrow'></span></th>";
-            }
+				}
+				echo "<th id='$question' class='$class'>$text<span class='sort-arrow'></span></th>";
+			}
 			
-            $jobs = $arr->Jobs->Job;
-            $odd = 0;
-            $class="";
-            //$qh = KenexaJobQuestions::getQuestionsHash();
-            $num = 1;
-            foreach ($jobs as $job) {			
-                if($odd) $class="odd";
-                else $class="";
-                echo "<tr id='result-$num'>";
-                echo "<td class='$class title'>". "<span onclick='showDetails(this);return false;' class='arrow'></span><a href='{$job->JobDetailLink}' onclick='showDetails(this);return false;' class='details-link'>".$job->Question[KenexaJobData::JOB_TITLE] . "</a></td>";
-                echo "<td class='$class location'>". $job->Question[KenexaJobData::LOCATION] . "</td>";
-                echo "<td class='$class division'>". $job->Question[KenexaJobData::DIVISION] . "</td>";
-                echo "<td class='$class industry' onMouseOver='showTruncated(this);' onMouseOut='hideTruncated(this);' >". $this->Truncate($job->Question[KenexaJobData::INDUSTRY],45,true) . "</td>";
-                echo "<td class='$class type'>". $job->Question[KenexaJobData::POSITION_TYPE] . "</td>";
-                echo "<td class='$class req'>". $job->Question[KenexaJobData::REQUISITION_NO] . "</td>";
-                echo "<td class='$class updated last'>". $job->LastUpdated . "</td>";
-                echo "</tr>";
-                echo "<tr id='result-$num-details' class='details'>";
-                echo "<td colspan='7' class='details-cell'>";
-                echo $this->Truncate($job->JobDescription,400, false);
-                echo "<div class='view-full'><a href='{$job->JobDetailLink}'>View Full Description</a></div>";
-                echo "</td>";
-                echo "</tr>";
-                $odd ^= 1;
-                $num ++;
-            }
-            echo "<tr class='details details-visible'><td colspan='7' class='details-cell'><div style='width:100%;'></div></td></tr>";
-
-            echo "</table>";
-        }else {
-            // If we get here, there's either no questions or maybe an error in the search.
-        }
+			$jobs = $arr->Jobs->Job;
+			$odd = 0;
+			$class="";
+			//$qh = KenexaJobQuestions::getQuestionsHash();
+			$num = 1;
+			foreach ($jobs as $job) {			
+				if($odd) $class="odd";
+				else $class="";
+				echo "<tr id='result-$num'>";
+				echo "<td class='$class title'>". "<span onclick='showDetails(this);return false;' class='arrow'></span><a href='{$job->JobDetailLink}' onclick='showDetails(this);return false;' class='details-link'>".$job->Question[KenexaJobData::JOB_TITLE] . "</a></td>";
+				echo "<td class='$class location'>". $job->Question[KenexaJobData::LOCATION] . "</td>";
+				echo "<td class='$class division'>". $job->Question[KenexaJobData::DIVISION] . "</td>";
+				echo "<td class='$class industry' onMouseOver='showTruncated(this);' onMouseOut='hideTruncated(this);' >". $this->Truncate($job->Question[KenexaJobData::INDUSTRY],45,true) . "</td>";
+				echo "<td class='$class type'>". $job->Question[KenexaJobData::POSITION_TYPE] . "</td>";
+				echo "<td class='$class req'>". $job->Question[KenexaJobData::REQUISITION_NO] . "</td>";
+				echo "<td class='$class updated last'>". $job->LastUpdated . "</td>";
+				echo "</tr>";
+				echo "<tr id='result-$num-details' class='details'>";
+				echo "<td colspan='7' class='details-cell'>";
+				echo $this->Truncate($job->JobDescription,400, false);
+				echo "<div class='view-full'><a href='{$job->JobDetailLink}'>View Full Description</a></div>";
+				echo "</td>";
+				echo "</tr>";
+				$odd ^= 1;
+				$num ++;
+			}
+			echo "</table>";
+		} else {
+			echo '<div class="no-results"><div class="results-count">Your Search returned 0 Results</div></div>'; // Hack to show an extra row to prevent IE from resizing columns
+		}
+	}else {
+	// If we get here, there's either no questions or maybe an error in the search.
+	}
   }
   
   function Truncate($string, $length, $output_truncated) {
