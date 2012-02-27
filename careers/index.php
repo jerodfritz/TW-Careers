0dee0794653c27e1d91b17f3385b99e8627f52fc
Jerod <jerod@centogram.com>
2012-02-27 14:26:59 -0500

    Stay Connected Link make not open in new window

--- careers/index.php
+++ careers/index.php
@@ -79,7 +79,7 @@ include '../includes/careers-head.php';
                    </button>
                    <div id="third-width-box-last" class="third-width-box third-width-box-last">
                      <div class="wrapper"><div class="inner">
-                       <a href="working-with-us/stay-connected/" target="_blank" title="Stay Connected" ><img src="<?php print $dir ?>images/buttons/arrow.png" class="connect-button" title="Stay Connected Bullet Arrow" alt="Stay Connected Bullet Arrow" /></a>
+                       <a href="working-with-us/stay-connected/" title="Stay Connected" ><img src="<?php print $dir ?>images/buttons/arrow.png" class="connect-button" title="Stay Connected Bullet Arrow" alt="Stay Connected Bullet Arrow" /></a>
                        <a href="http://www.facebook.com/TimeWarner" target="_blank" title="Connect Facebook" ><img src="<?php print $dir ?>images/buttons/facebook.png" class="connect-button" title="Connect Facebook" alt="Connect Facebook" /></a>
                        <a href="http://www.youtube.com/user/TimeWarnerCable" target="_blank" title="Connect YouTube" ><img src="<?php print $dir ?>images/buttons/youtube.png" class="connect-button" title="Connect YouTube" alt="Connect YouTube" /></a>
                        <a href="https://twitter.com/twxcorp" target="_blank" title="Connect Twitter" ><img src="<?php print $dir ?>images/buttons/twitter.png" class="connect-button" title="Connect Twitter" alt="Connect Twitter" /></a>
